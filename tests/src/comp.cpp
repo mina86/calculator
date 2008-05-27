c89d9017c02180569ba9cf0baf08d315204f9344
mina86 <mina86>
2008-05-27 18:15:06 +0000

    + changed to long double (configurable) + compare() renamed to differ() + differ() ignores values if both less then 1e-10 + minor formatting chagnes

--- tests/src/comp.cpp
+++ tests/src/comp.cpp
@@ -3,45 +3,50 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
 
-static inline double abs(double x) {
+#if 1
+#  if __GNUC__
+	__extension__
+#  endif
+	typedef long double real;
+#else
+	typedef double real;
+#endif
+
+
+static inline real abs(real x) {
 	return x < 0 ? -x : x;
 }
 
-static inline bool compare(double v1, double v2, double pct) {
-	return abs((v1 - v2) / v1) > pct;
+
+static inline bool differ(real v1, real v2, real pct) {
+	/* If both v1 and v2 are *very* small it's likelly that due to
+	   rounding they will differ so ignore such situations. */
+	return (v1 >= 1e-10 || v2 >= 1e-10) && abs((v1 - v2) / v1) >= pct;
 }
 
-int main(int argc, char *argv[])
-{
-	double pct = 0.1;
-	if(argc < 3)
-	{
-		cout << "usage: ./comp file1 file2 [pct]\n"
-			"pct - difference (in %) allowed in compared values";
+
+int main(int argc, char *argv[]) {
+	real pct = 0.1;
+	if (argc < 3) {
+		std::cout << "usage: " << argv[0] << " <file1> <file2> [<pct>]\n"
+			"  pct - difference (in %) allowed in compared values";
 		return 0;
 	}
-	if(argc >= 4)
-	{
-	    pct = strtod(argv[3], 0);
+	if (argc >= 4) {
+		pct = strtod(argv[3], 0);
 	}
 
-	ifstream file1(argv[1]);
-	ifstream file2(argv[2]);
-
-	cout << fixed;
+	std::ifstream file1(argv[1]), file2(argv[2]);
 
 	bool foundDiff = false;
-	int lineCount = 0;
-	double var1, var2;
+	unsigned lineCount = 0;
+	real var1, var2;
 	while (file1 >> var1 && file2 >> var2) {
 		++lineCount;
-		if(compare(var1, var2, pct))
-		{
-			cerr << "Found difference at line: " << lineCount
-			     << " values: " << var1 << " != " << var2
-			     << " Diff: " << var1-var2 << endl;
+		if (differ(var1, var2, pct)) {
+			std::cout << lineCount << ": found difference (" << var1
+			          << " - " << var2 << " = " << (var1 - var2) << ")\n";
 			foundDiff = true;
 		}
 	}
