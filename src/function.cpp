aaeeca8b48dc9b7a4d3fb8c1199e963f151fcc85
mina86 <mina86>
2008-06-05 20:37:04 +0000

    + loop replaced with std::copy

--- src/function.cpp
+++ src/function.cpp
@@ -1,6 +1,6 @@
 /** \file
  * Function class virtual methods definitions.
- * $Id: function.cpp,v 1.1 2008/04/21 10:14:54 mina86 Exp $
+ * $Id: function.cpp,v 1.2 2008/06/05 20:37:04 mina86 Exp $
  */
 #include "function.hpp"
 #include "expression.hpp"
@@ -14,10 +14,9 @@ real Function::execute(Environment &env,
                        const std::vector<Expression*> &args) const {
 	std::vector<Expression*>::const_iterator it(args.begin()), e(args.end());
 	Arguments a(args.size());
-	Arguments::iterator i(a.begin());
-	for (; it != e; ++it, ++i) {
-		*i = (*it)->execute(env);
-	}
+	std::copy(Expression::executor_iterator(args.begin(), env),
+	          Expression::executor_iterator(args.end(), env),
+	          a.begin());
 	return execute(env, a);
 }
 
